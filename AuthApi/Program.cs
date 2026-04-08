97c180b52786d145f43852fe743dec419543312e
wmcod <142944866+wfmacena@users.noreply.github.com>
2026-04-08 15:50:47 -0300

    add render db migrate

--- AuthApi/Program.cs
+++ AuthApi/Program.cs
@@ -27,6 +27,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Swagger (IMPORTANTE)
 app.UseSwagger();
 app.UseSwaggerUI();
