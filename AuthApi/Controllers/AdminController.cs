887bea4bf734939e351f17261a9cdf53e6b96112
wmcod <142944866+wfmacena@users.noreply.github.com>
2026-04-08 17:36:07 -0300

    adicionado interface de login

---
+++ AuthApi/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using AuthApi.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AdminController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] AdminLoginRequest request)
+    {
+        var adminUser = "plugadmin";
+        var adminPass = "123456wm";
+
+        if (request.Username != adminUser || request.Password != adminPass)
+        {
+            return Unauthorized(new
+            {
+                success = false,
+                error = "Usuário ou senha inválidos"
+            });
+        }
+
+        return Ok(new
+        {
+            success = true,
+            token = "admin-logado"
+        });
+    }
+}
